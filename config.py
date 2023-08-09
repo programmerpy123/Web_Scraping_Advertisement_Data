b6c3f5481d2ab116c79c321d965b9b7dcbafb476
amirreza <amirrezamddi790@gmail.com>
2023-08-09 16:55:35 +0330

    implement selenuim from login part

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ BASE_LINK = 'https://{}.craigslist.org/search/hhh#search=1~gallery~{}~0'
 CITIES = ['paris', 'berlin']
 STORAGE_TYPE = 'mongo' # choices => file , mongo
 crawl = 'find'
+login_site_address = 'https://accounts.craigslist.org/login'
+site_email ="amirrezamddi790@gmail.com"
+site_password = "Amirreza123!!"
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from crawl import LinkCrawler
 from config import CITIES
 from crawl import DataCrawler,ImageDownloader
-
+from login import  start
 
 if __name__ == "__main__":
     switch = sys.argv[1]
